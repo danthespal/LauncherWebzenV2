78389111d872cbf2e1285c916ee5cb8414859af1
danthespal <danielplanteskole@gmail.com>
2021-04-15 00:29:24 +0300

    simplify ListDownloader.cs

--- Source/ListDownloader.cs
+++ Source/ListDownloader.cs
@@ -8,35 +8,39 @@ using System.Windows.Forms;
 
 namespace LauncherWebzenV2.Source
 {
-  internal class ListDownloader
+    internal class ListDownloader
     {
         public static void DownloadList()
         {
             Common.ChangeStatus("LISTDOWNLOAD");
             Common.DisableStart();
             BackgroundWorker backgroundWorker = new BackgroundWorker();
-            backgroundWorker.DoWork += new DoWorkEventHandler(ListDownloader.backgroundWorker_DoWork);
-            backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(ListDownloader.backgroundWorker_RunWorkerCompleted);
+            backgroundWorker.DoWork += new DoWorkEventHandler(BackgroundWorker_DoWork);
+            backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackgroundWorker_RunWorkerCompleted);
             
             if (backgroundWorker.IsBusy)
             {
-                int num = (int) MessageBox.Show(Texts.GetText("UNKNOWNERROR", (object) "DownloadList isBusy"));
+                int num = (int)MessageBox.Show(Texts.GetText("UNKNOWNERROR", (object)"DownloadList isBusy"));
                 Application.Exit();
             }
             else
+            {
                 backgroundWorker.RunWorkerAsync();
+            }
         }
 
-        private static void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        private static void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
             StreamReader streamReader = new StreamReader(new WebClient().OpenRead(Import.ServerURL + Import.PatchlistName));
             while (!streamReader.EndOfStream)
+            {
                 ListProcessor.AddFile(streamReader.ReadLine());
+            }
         }
 
-        private static void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        private static void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-          FileChecker.CheckFiles();
+            FileChecker.CheckFiles();
         }
     }
 }
