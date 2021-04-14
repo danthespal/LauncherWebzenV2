effc1fe7ad9682a3bcce91dbf43aa41f94c30b70
danthespal <danielplanteskole@gmail.com>
2021-04-15 00:21:34 +0300

    simplify Computer.cs

--- Source/Computer.cs
+++ Source/Computer.cs
@@ -7,8 +7,19 @@ namespace LauncherWebzenV2.Source
 {
     internal class Computer
     {
-        public static long Compute(long Size) => Size * 100L / Import.fullSize;
-        public static double ComputeDownloadSize(double Size) => Size / 1024.0 / 1024.0;
-        public static double ComputeDownloadSpeed(double Size, Stopwatch stopWatch) => Size / 1024.0 / stopWatch.Elapsed.TotalSeconds;
+        public static long Compute(long Size)
+        {
+            return Size * 100L / Import.fullSize;
+        }
+
+        public static double ComputeDownloadSize(double Size)
+        {
+            return Size / 1024.0 / 1024.0;
+        }
+
+        public static double ComputeDownloadSpeed(double Size, Stopwatch stopWatch)
+        {
+            return Size / 1024.0 / stopWatch.Elapsed.TotalSeconds;
+        }
     }
 }
