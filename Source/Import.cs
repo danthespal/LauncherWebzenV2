58b08953187cb91247e79268f09a5eec25f66aa7
danthespal <danielplanteskole@gmail.com>
2021-04-15 00:27:16 +0300

    simplify Import.cs

--- Source/Import.cs
+++ Source/Import.cs
@@ -13,68 +13,68 @@ namespace LauncherWebzenV2.Source
         public static string PatchlistName = "";
         public static string ExecutableName = "Main.exe";
         public static string MutexName = "";
-        public static Mutex Mutex = (Mutex) null;
+        public static Mutex Mutex = null;
         public static string webPanelURL = "";
         public static string windowName = "Launcher";
         public static int LauncherLanguage = 0;
         public static Main gMain;
-        public static List<Import.File> Files = new List<Import.File>();
+        public static List<File> Files = new List<File>();
         public static List<string> OldFiles = new List<string>();
         public static long fullSize;
         public static long completeSize;
-        public static IDictionary<string, string> Lang = (IDictionary<string, string>) new Dictionary<string, string>()
+        public static IDictionary<string, string> Lang = new Dictionary<string, string>()
         {
             {
-              "Eng",
-              "English"
+                "Eng",
+                "English"
             },
             {
-              "Spn",
-              "Español"
+                "Spn",
+                "Español"
             },
             {
-              "Por",
-              "Português"
+                "Por",
+                "Português"
             }
         };
-        public static IDictionary<int, string> Resolution = (IDictionary<int, string>) new Dictionary<int, string>()
+        public static IDictionary<int, string> Resolution = new Dictionary<int, string>()
         {
-          {
-            0,
-            "640x480"
-          },
-          {
-            1,
-            "800x600"
-          },
-          {
-            2,
-            "1024x768"
-          },
-          {
-            3,
-            "1280x1024"
-          },
-          {
-            4,
-            "1366x768"
-          },
-          {
-            5,
-            "1440x900"
-          },
-          {
-            6,
-            "1600x900"
-          },
-          {
-            7,
-            "1680x1050"
-          },
-          {
-            8,
-            "1920x1080"
-          }
+            {
+                 0,
+                "640x480"
+            },
+            {
+                1,
+                "800x600"
+            },
+            {
+                2,
+                "1024x768"
+            },
+            {
+                3,
+                "1280x1024"
+            },
+            {
+                4,
+                "1366x768"
+            },
+            {
+                5,
+                "1440x900"
+            },
+            {
+                6,
+                "1600x900"
+            },
+            {
+                7,
+                "1680x1050"
+            },
+            {
+                8,
+                "1920x1080"
+            }
         };
 
         public struct File
