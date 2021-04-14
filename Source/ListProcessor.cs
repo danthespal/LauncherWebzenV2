f3074fa98e9442918fe35d62bc8a0f7c999fe130
danthespal <danielplanteskole@gmail.com>
2021-04-15 00:30:09 +0300

    simplify ListProcessor.cs

--- Source/ListProcessor.cs
+++ Source/ListProcessor.cs
@@ -7,11 +7,14 @@ namespace LauncherWebzenV2.Source
 {
     internal class ListProcessor
     {
-        public static void AddFile(string File) => Import.Files.Add(new Import.File()
+        public static void AddFile(string File)
         {
-            Name = File.Split(' ')[0],
-            Hash = File.Split(' ')[1],
-            Size = Convert.ToInt64(File.Split(' ')[2])
-        });
+            Import.Files.Add(new Import.File()
+            {
+                Name = File.Split(' ')[0],
+                Hash = File.Split(' ')[1],
+                Size = Convert.ToInt64(File.Split(' ')[2])
+            });
+        }
     }
 }
